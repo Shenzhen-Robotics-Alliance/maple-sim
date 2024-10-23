32da66bd0843cff1c0663c358e6519bb22d87145
thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>
2024-10-23 13:12:33 -0500

    Fixed vendordep publishing

--- project/src/main/java/org/ironmaple/simulation/seasonspecific/crescendo2024/Arena2024Crescendo.java
+++ project/src/main/java/org/ironmaple/simulation/seasonspecific/crescendo2024/Arena2024Crescendo.java
@@ -1,5 +1,7 @@
 package org.ironmaple.simulation.seasonspecific.crescendo2024;
 
+import static org.ironmaple.utils.FieldMirroringUtils.toCurrentAllianceTranslation;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -8,163 +10,105 @@ import edu.wpi.first.wpilibj.Timer;
 import org.ironmaple.simulation.SimulatedArena;
 import org.ironmaple.simulation.gamepieces.GamePieceOnFieldSimulation;
 
-import static org.ironmaple.utils.FieldMirroringUtils.toCurrentAllianceTranslation;
-
 public class Arena2024Crescendo extends SimulatedArena {
-    /**
-     * the obstacles on the 2024 competition field
-     * */
-    public static final class CrescendoFieldObstaclesMap extends FieldMap {
-        private static final double FIELD_WIDTH = 16.54;
-        public CrescendoFieldObstaclesMap() {
-            super();
+  /** the obstacles on the 2024 competition field */
+  public static final class CrescendoFieldObstaclesMap extends FieldMap {
+    private static final double FIELD_WIDTH = 16.54;
 
-            //left wall
-            super.addBorderLine(
-                    new Translation2d(0, 1),
-                    new Translation2d(0, 4.51)
-            );
-            super.addBorderLine(
-                    new Translation2d(0, 4.51),
-                    new Translation2d(0.9, 5)
-            );
+    public CrescendoFieldObstaclesMap() {
+      super();
 
-            super.addBorderLine(
-                    new Translation2d(0.9, 5),
-                    new Translation2d(0.9, 6.05)
-            );
+      // left wall
+      super.addBorderLine(new Translation2d(0, 1), new Translation2d(0, 4.51));
+      super.addBorderLine(new Translation2d(0, 4.51), new Translation2d(0.9, 5));
 
-            super.addBorderLine(
-                    new Translation2d(0.9, 6.05),
-                    new Translation2d(0, 6.5)
-            );
-            super.addBorderLine(
-                    new Translation2d(0, 6.5),
-                    new Translation2d(0, 8.2)
-            );
+      super.addBorderLine(new Translation2d(0.9, 5), new Translation2d(0.9, 6.05));
 
+      super.addBorderLine(new Translation2d(0.9, 6.05), new Translation2d(0, 6.5));
+      super.addBorderLine(new Translation2d(0, 6.5), new Translation2d(0, 8.2));
 
-            // upper wall
-            super.addBorderLine(
-                    new Translation2d(0, 8.12),
-                    new Translation2d(FIELD_WIDTH, 8.12)
-            );
+      // upper wall
+      super.addBorderLine(new Translation2d(0, 8.12), new Translation2d(FIELD_WIDTH, 8.12));
 
-            // righter wall
-            super.addBorderLine(
-                    new Translation2d(FIELD_WIDTH, 1),
-                    new Translation2d(FIELD_WIDTH, 4.51)
-            );
-            super.addBorderLine(
-                    new Translation2d(FIELD_WIDTH, 4.51),
-                    new Translation2d(FIELD_WIDTH-0.9, 5)
-            );
-            super.addBorderLine(
-                    new Translation2d(FIELD_WIDTH-0.9, 5),
-                    new Translation2d(FIELD_WIDTH-0.9, 6.05)
-            );
-            super.addBorderLine(
-                    new Translation2d(FIELD_WIDTH-0.9, 6.05),
-                    new Translation2d(FIELD_WIDTH, 6.5)
-            );
-            super.addBorderLine(
-                    new Translation2d(FIELD_WIDTH, 6.5),
-                    new Translation2d(FIELD_WIDTH, 8.2)
-            );
+      // righter wall
+      super.addBorderLine(new Translation2d(FIELD_WIDTH, 1), new Translation2d(FIELD_WIDTH, 4.51));
+      super.addBorderLine(
+          new Translation2d(FIELD_WIDTH, 4.51), new Translation2d(FIELD_WIDTH - 0.9, 5));
+      super.addBorderLine(
+          new Translation2d(FIELD_WIDTH - 0.9, 5), new Translation2d(FIELD_WIDTH - 0.9, 6.05));
+      super.addBorderLine(
+          new Translation2d(FIELD_WIDTH - 0.9, 6.05), new Translation2d(FIELD_WIDTH, 6.5));
+      super.addBorderLine(new Translation2d(FIELD_WIDTH, 6.5), new Translation2d(FIELD_WIDTH, 8.2));
 
-            // lower wall
-            super.addBorderLine(
-                    new Translation2d(1.92, 0),
-                    new Translation2d(FIELD_WIDTH-1.92, 0)
-            );
+      // lower wall
+      super.addBorderLine(new Translation2d(1.92, 0), new Translation2d(FIELD_WIDTH - 1.92, 0));
 
-            // red source wall
-            super.addBorderLine(
-                    new Translation2d(1.92, 0),
-                    new Translation2d(0, 1)
-            );
+      // red source wall
+      super.addBorderLine(new Translation2d(1.92, 0), new Translation2d(0, 1));
 
-            // blue source wall
-            super.addBorderLine(
-                    new Translation2d(FIELD_WIDTH-1.92, 0),
-                    new Translation2d(FIELD_WIDTH, 1)
-            );
+      // blue source wall
+      super.addBorderLine(
+          new Translation2d(FIELD_WIDTH - 1.92, 0), new Translation2d(FIELD_WIDTH, 1));
 
-            // blue state
-            super.addRectangularObstacle(
-                    0.35, 0.35,
-                    new Pose2d(3.4, 4.1, new Rotation2d())
-            );
-            super.addRectangularObstacle(
-                    0.35, 0.35,
-                    new Pose2d(5.62, 4.1-1.28, Rotation2d.fromDegrees(30))
-            );
-            super.addRectangularObstacle(
-                    0.35, 0.35,
-                    new Pose2d(5.62, 4.1+1.28, Rotation2d.fromDegrees(60))
-            );
+      // blue state
+      super.addRectangularObstacle(0.35, 0.35, new Pose2d(3.4, 4.1, new Rotation2d()));
+      super.addRectangularObstacle(
+          0.35, 0.35, new Pose2d(5.62, 4.1 - 1.28, Rotation2d.fromDegrees(30)));
+      super.addRectangularObstacle(
+          0.35, 0.35, new Pose2d(5.62, 4.1 + 1.28, Rotation2d.fromDegrees(60)));
 
-            // red stage
-            super.addRectangularObstacle(
-                    0.35, 0.35,
-                    new Pose2d(FIELD_WIDTH-3.4, 4.1, new Rotation2d())
-            );
-            super.addRectangularObstacle(
-                    0.35, 0.35,
-                    new Pose2d(FIELD_WIDTH-5.62, 4.1-1.28, Rotation2d.fromDegrees(60))
-            );
-            super.addRectangularObstacle(
-                    0.35, 0.35,
-                    new Pose2d(FIELD_WIDTH-5.62, 4.1+1.28, Rotation2d.fromDegrees(30))
-            );
-        }
+      // red stage
+      super.addRectangularObstacle(
+          0.35, 0.35, new Pose2d(FIELD_WIDTH - 3.4, 4.1, new Rotation2d()));
+      super.addRectangularObstacle(
+          0.35, 0.35, new Pose2d(FIELD_WIDTH - 5.62, 4.1 - 1.28, Rotation2d.fromDegrees(60)));
+      super.addRectangularObstacle(
+          0.35, 0.35, new Pose2d(FIELD_WIDTH - 5.62, 4.1 + 1.28, Rotation2d.fromDegrees(30)));
     }
+  }
 
-    private static final Translation2d[] NOTE_INITIAL_POSITIONS = new Translation2d[] {
-            new Translation2d(2.9, 4.1),
-            new Translation2d(2.9, 5.55),
-            new Translation2d(2.9, 7),
+  private static final Translation2d[] NOTE_INITIAL_POSITIONS =
+      new Translation2d[] {
+        new Translation2d(2.9, 4.1),
+        new Translation2d(2.9, 5.55),
+        new Translation2d(2.9, 7),
+        new Translation2d(8.27, 0.75),
+        new Translation2d(8.27, 2.43),
+        new Translation2d(8.27, 4.1),
+        new Translation2d(8.27, 5.78),
+        new Translation2d(8.27, 7.46),
+        new Translation2d(13.64, 4.1),
+        new Translation2d(13.64, 5.55),
+        new Translation2d(13.64, 7),
+      };
 
-            new Translation2d(8.27, 0.75),
-            new Translation2d(8.27, 2.43),
-            new Translation2d(8.27, 4.1),
-            new Translation2d(8.27, 5.78),
-            new Translation2d(8.27, 7.46),
+  public Arena2024Crescendo() {
+    super(new CrescendoFieldObstaclesMap());
+  }
 
-            new Translation2d(13.64, 4.1),
-            new Translation2d(13.64, 5.55),
-            new Translation2d(13.64, 7),
-    };
-    public Arena2024Crescendo() {
-        super(new CrescendoFieldObstaclesMap());
-    }
+  @Override
+  public void placeGamePiecesOnField() {
+    for (Translation2d notePosition : NOTE_INITIAL_POSITIONS)
+      super.addGamePiece(new CrescendoNoteOnField(notePosition));
+  }
 
-    @Override
-    public void placeGamePiecesOnField() {
-        for (Translation2d notePosition:NOTE_INITIAL_POSITIONS)
-            super.addGamePiece(new CrescendoNoteOnField(notePosition));
-    }
+  private static final Translation2d BLUE_SOURCE_POSITION = new Translation2d(15.6, 0.8);
+  private double previousThrowTimeSeconds = 0;
 
-    private static final Translation2d BLUE_SOURCE_POSITION = new Translation2d(15.6, 0.8);
-    private double previousThrowTimeSeconds = 0;
-    @Override
-    public void competitionPeriodic() {
-        if (!DriverStation.isTeleopEnabled()) return;
+  @Override
+  public void competitionPeriodic() {
+    if (!DriverStation.isTeleopEnabled()) return;
 
-        if (Timer.getFPGATimestamp() - previousThrowTimeSeconds < 1) return;
+    if (Timer.getFPGATimestamp() - previousThrowTimeSeconds < 1) return;
 
-        final Translation2d sourcePosition = toCurrentAllianceTranslation(BLUE_SOURCE_POSITION);
-        /* if there is any game-piece 0.5 meters within the human player station, we don't throw a new note */
-        for (GamePieceOnFieldSimulation gamePiece:super.gamePieces)
-            if (gamePiece instanceof CrescendoNoteOnField
-                    && gamePiece
-                    .getPoseOnField()
-                    .getTranslation()
-                    .getDistance(sourcePosition)
-                    < 1) return;
+    final Translation2d sourcePosition = toCurrentAllianceTranslation(BLUE_SOURCE_POSITION);
+    /* if there is any game-piece 0.5 meters within the human player station, we don't throw a new note */
+    for (GamePieceOnFieldSimulation gamePiece : super.gamePieces)
+      if (gamePiece instanceof CrescendoNoteOnField
+          && gamePiece.getPoseOnField().getTranslation().getDistance(sourcePosition) < 1) return;
 
-        /* otherwise, place a note */
-        addGamePiece(new CrescendoNoteOnField(sourcePosition));
-        previousThrowTimeSeconds = Timer.getFPGATimestamp();
-    }
+    /* otherwise, place a note */
+    addGamePiece(new CrescendoNoteOnField(sourcePosition));
+    previousThrowTimeSeconds = Timer.getFPGATimestamp();
+  }
 }
--- project/src/main/java/org/ironmaple/utils/mathutils/SwerveStateProjection.java
+++ project/src/main/java/org/ironmaple/utils/mathutils/SwerveStateProjection.java
@@ -5,20 +5,22 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 
 public class SwerveStateProjection {
-    /**
-     * Projects the swerve module speed onto the direction of the current swerve facing.
-     * @param swerveSpeed The current speed and direction of the swerve module.
-     * @param currentSwerveFacing The desired direction to project onto.
-     * @return The projected speed in the direction of currentSwerveFacing.
-     */
-    public static double project(SwerveModuleState swerveSpeed, Rotation2d currentSwerveFacing) {
-        // Get the angle of the swerve module's current direction
-        Rotation2d swerveModuleAngle = swerveSpeed.angle;
+  /**
+   * Projects the swerve module speed onto the direction of the current swerve facing.
+   *
+   * @param swerveSpeed The current speed and direction of the swerve module.
+   * @param currentSwerveFacing The desired direction to project onto.
+   * @return The projected speed in the direction of currentSwerveFacing.
+   */
+  public static double project(SwerveModuleState swerveSpeed, Rotation2d currentSwerveFacing) {
+    // Get the angle of the swerve module's current direction
+    Rotation2d swerveModuleAngle = swerveSpeed.angle;
 
-        // Calculate the cosine of the angle difference between swerve module direction and the desired direction
-        double cosTheta = Math.cos(swerveModuleAngle.minus(currentSwerveFacing).getRadians());
+    // Calculate the cosine of the angle difference between swerve module direction and the desired
+    // direction
+    double cosTheta = Math.cos(swerveModuleAngle.minus(currentSwerveFacing).getRadians());
 
-        // Scale the speed by the cosine value to get the projection
-        return swerveSpeed.speedMetersPerSecond * cosTheta;
-    }
+    // Scale the speed by the cosine value to get the projection
+    return swerveSpeed.speedMetersPerSecond * cosTheta;
+  }
 }

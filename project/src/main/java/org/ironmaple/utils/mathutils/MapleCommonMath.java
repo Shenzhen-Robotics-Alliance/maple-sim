0d69e0769e3d3db38bd2ccbc4454e0ca8553466e
caTrixLiu <lyr20070621@gmail.com>
2024-10-24 10:14:07 +0800

    removed redundant code in maple common math

--- project/src/main/java/org/ironmaple/utils/mathutils/MapleCommonMath.java
+++ project/src/main/java/org/ironmaple/utils/mathutils/MapleCommonMath.java
@@ -1,20 +1,9 @@
 package org.ironmaple.utils.mathutils;
 
-import edu.wpi.first.math.geometry.Rotation3d;
 import java.util.Random;
 
 public class MapleCommonMath {
   private static final Random random = new Random();
-
-  public static double linearInterpretationWithBounding(
-      double x1, double y1, double x2, double y2, double x) {
-    final double minX = Math.min(x1, x2), maxX = Math.max(x1, x2);
-    return linearInterpretation(x1, y1, x2, y2, Math.min(maxX, Math.max(minX, x)));
-  }
-
-  public static double linearInterpretation(double x1, double y1, double x2, double y2, double x) {
-    return y1 + (x - x1) * (y2 - y1) / (x2 - x1);
-  }
 
   /**
    * using the random number generator of a fixed seed, generate the next random normal variable
@@ -33,15 +22,5 @@ public class MapleCommonMath {
 
   public static double constrainMagnitude(double value, double maxMagnitude) {
     return Math.copySign(Math.min(Math.abs(value), Math.abs(maxMagnitude)), value);
-  }
-
-  public static String printRotation3d(Rotation3d rotation3d) {
-    return "rotation 3d object with value: "
-        + rotation3d.getQuaternion()
-        + String.format(
-            "\nand roll %.2f deg, pitch %.2f deg, yaw %.2f deg",
-            Math.toDegrees(rotation3d.getX()),
-            Math.toDegrees(rotation3d.getY()),
-            Math.toDegrees(rotation3d.getZ()));
   }
 }

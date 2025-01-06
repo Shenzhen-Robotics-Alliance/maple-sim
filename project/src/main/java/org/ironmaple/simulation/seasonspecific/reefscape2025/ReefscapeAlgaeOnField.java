e351f91b24f20b26e0ac98b8fee0d977b73ff737
caTrixLiu <lyr20070621@gmail.com>
2025-01-06 22:38:48 +0800

    added coral and algae game piece

--- project/src/main/java/org/ironmaple/simulation/seasonspecific/reefscape2025/ReefscapeAlgaeOnField.java
+++ project/src/main/java/org/ironmaple/simulation/seasonspecific/reefscape2025/ReefscapeAlgaeOnField.java
@@ -1,3 +1,18 @@
 package org.ironmaple.simulation.seasonspecific.reefscape2025;
 
-public class ReefscapeAlgaeOnField {}
+import static edu.wpi.first.units.Units.*;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import org.dyn4j.geometry.Circle;
+import org.ironmaple.simulation.gamepieces.GamePieceOnFieldSimulation;
+
+public class ReefscapeAlgaeOnField extends GamePieceOnFieldSimulation {
+    public static final GamePieceInfo REEFSCAPE_ALGAE_INFO =
+            new GamePieceInfo("Algae", new Circle(0.176), Inches.of(16), Kilograms.of(0.4), 1.2, 5, 0.8);
+
+    public ReefscapeAlgaeOnField(Translation2d initialPosition) {
+        super(REEFSCAPE_ALGAE_INFO, new Pose2d(initialPosition, new Rotation2d()));
+    }
+}
--- project/src/main/java/org/ironmaple/simulation/seasonspecific/reefscape2025/ReefscapeCoral.java
+++ project/src/main/java/org/ironmaple/simulation/seasonspecific/reefscape2025/ReefscapeCoral.java
@@ -1,3 +1,16 @@
 package org.ironmaple.simulation.seasonspecific.reefscape2025;
 
-public class ReefscapeCoral {}
+import static edu.wpi.first.units.Units.*;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import org.dyn4j.geometry.Rectangle;
+import org.ironmaple.simulation.gamepieces.GamePieceOnFieldSimulation;
+
+public class ReefscapeCoral extends GamePieceOnFieldSimulation {
+    public static final GamePieceInfo REEFSCAPE_CORAL_INFO =
+            new GamePieceInfo("Coral", new Rectangle(0.3, 0.11), Meters.of(0.11), Kilograms.of(0.3), 1.8, 4, 0.3);
+
+    public ReefscapeCoral(Pose2d initialPose) {
+        super(REEFSCAPE_CORAL_INFO, initialPose);
+    }
+}

a3e16bb605b835c22119b1357977a5db57fe8d02
caTrixLiu <lyr20070621@gmail.com>
2025-01-10 19:59:06 +0800

    installed maple-sim to project

---
+++ templates/CTRE Swerve with maple-sim/src/main/java/frc/robot/utils/MapleSimDrivetrain.java
@@ -0,0 +1,15 @@
+package frc.robot.utils;
+
+import org.ironmaple.simulation.drivesims.SwerveDriveSimulation;
+import org.ironmaple.simulation.drivesims.SwerveModuleSimulation;
+
+public class MapleSimDrivetrain {
+    private final SwerveDriveSimulation simulation;
+    private final SwerveModuleSimulation[] modules;
+    public MapleSimDrivetrain() {
+
+    }
+
+    protected static class MapleSimModule {
+    }
+}

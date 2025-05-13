ad721ac818f22d821ce18d36a00cfcb006ddd86d
caTrixLiu <lyr20070621@gmail.com>
2025-05-13 20:43:58 +0800

    added coral in intake visualization

--- examples/ReefScape-Design-Concept-Validation/src/main/java/frc/robot/subsystems/Intake.java
+++ examples/ReefScape-Design-Concept-Validation/src/main/java/frc/robot/subsystems/Intake.java
@@ -3,6 +3,8 @@ package frc.robot.subsystems;
 import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Meters;
 
+import dev.doglog.DogLog;
+import edu.wpi.first.math.geometry.*;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -56,7 +58,7 @@ public class Intake extends SubsystemBase {
 
     @Override
     public void periodic() {
-        if (turnedOn & intakeSimulation.getGamePiecesAmount() >= 1) coralPosition += 0.02 / coralIndexingSeconds;
+        if (turnedOn & intakeSimulation.getGamePiecesAmount() > 0) coralPosition += 0.02 / coralIndexingSeconds;
 
         double desiredPosition = turnedOn ? intakeState : idleState;
         state = profile.calculate(0.02, state, new TrapezoidProfile.State(desiredPosition, 0.0));
@@ -65,7 +67,34 @@ public class Intake extends SubsystemBase {
 
         MechanismVisualizer.getInstance().setIntakeAngle(Degrees.of(state.position));
 
-        // TODO: visualize the Game Piece in the intake
+        DogLog.log("Intake/CoralInIntake", intakeSimulation.getGamePiecesAmount() > 0 ? getCoralInIntakePose() : null);
+    }
+
+    /**
+     * Visualizes the position of the coral inside the intake Uses a simple linear interpretation to display how much
+     * the coral have been processed
+     */
+    private Pose3d getCoralInIntakePose() {
+        //  Step0: Obtain drivetrain center pose from drive sim
+        Pose3d driveTrainCenterPose = new Pose3d(driveSim.getSimulatedDriveTrainPose());
+
+        // Step1: Calculate the pose of the coral IMMEDIATELY after being intake
+        // Step1.1: Apply a twist to the drive center pose to get the coral pose
+        Twist3d driveCenterToCoralStartingPose = new Twist3d(-0.55, 0.0, 0.15, 0.0, 0.0, 0.0);
+        Pose3d coralStartingPose = driveTrainCenterPose.exp(driveCenterToCoralStartingPose);
+        // Step1.2: Apply a rotation to the coral
+        Transform3d rotation = new Transform3d(new Translation3d(), new Rotation3d(0.0, Math.toRadians(-30.0), 0.0));
+        coralStartingPose = coralStartingPose.plus(rotation);
+
+        // Step2: Calculate the pose of the coral after being COMPLETELY PROCESSED
+        // (We use the same method as Step1).
+        Twist3d driveCenterToCoralEndingPose = new Twist3d(0.14, 0.0, 0.22, 0.0, 0.0, 0.0);
+        Pose3d coralEndingPose = driveTrainCenterPose.exp(driveCenterToCoralEndingPose);
+        rotation = new Transform3d(new Translation3d(), new Rotation3d(0.0, Math.toRadians(17.5), 0.0));
+        coralEndingPose = coralEndingPose.plus(rotation);
+
+        // Step3: Find the current position of the coral using linear interpretation
+        return coralStartingPose.interpolate(coralEndingPose, coralPosition);
     }
 
     public boolean obtainAvailableGamePiece() {

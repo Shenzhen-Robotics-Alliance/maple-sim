1269f56f06d8614549eaf6d6cdbd41dea4eaad43
Artemis <twiserjafla@gmail.com>
2025-08-29 11:35:13 -0600

    added scoring loggers

--- project/src/main/java/org/ironmaple/simulation/seasonspecific/reefscape2025/ReefscapeReefSimulation.java
+++ project/src/main/java/org/ironmaple/simulation/seasonspecific/reefscape2025/ReefscapeReefSimulation.java
@@ -17,7 +17,7 @@ import org.ironmaple.simulation.SimulatedArena;
  * does not directly handle scoring uses an array of {@link ReefscapeReefBranch} objects. However for all other purposes
  * this class behaves and can be used like a normal goal.
  */
-class ReefscapeReefSimulation implements SimulatedArena.Simulatable {
+public class ReefscapeReefSimulation implements SimulatedArena.Simulatable {
     protected final List<ReefscapeReefBranch> branches;
     public ReefscapeAlgaeOnField algae;
     private StructArrayPublisher<Pose3d> reefPub;
@@ -74,5 +74,32 @@ class ReefscapeReefSimulation implements SimulatedArena.Simulatable {
         for (ReefscapeReefBranch branch : branches) {
             branch.clear();
         }
+    }
+
+    /**
+     * Obtains the amount of <strong>CORAL</strong> held on the <strong>BRANCHES</strong>.
+     *
+     * <p>This method returns a 2D array of size 12 x 4, where each entry represents the number of
+     * <strong>CORAL</strong>s held on a particular branch.
+     *
+     * <p>The <strong>BRANCHES</strong> are tracked in FMS as A, B, C, D, E, F, G, H, I, J, K, L (as per the game
+     * manual), and are mapped to indices 0, 1, 2, ... in the array.
+     *
+     * <p>The [i][j] entry in the array represents the number of <strong>CORAL</strong>(s) held on the L<code>j-1</code>
+     * branch in the <code>i</code>th section.
+     *
+     * <p>For example, <code>getBranches()[2][3]</code> returns the number of CORALs held on L4 of Branch C.
+     *
+     * <p>Note that L2, L3, and L4 can only hold one <strong>CORAL</strong>, while L1 can hold up to two
+     * <strong>CORAL</strong>s.
+     *
+     * @return a 2D array where each entry represents the number of <strong>CORAL</strong> held on each branch
+     */
+    public int[][] getBranches() {
+        int[][] toReturn = new int[12][4];
+        for (ReefscapeReefBranch branch : branches) {
+            toReturn[branch.column][branch.level] = branch.getGamePieceCount();
+        }
+        return toReturn;
     }
 }
